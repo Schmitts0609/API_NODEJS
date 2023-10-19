6b0b59419e021d7718dec404e6e8e1aa1d5999b1
Cauã <caua.schmitt@hotmail.com.br>
2023-10-19 07:57:14 -0300

    rotas do projeto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
+const router = require('./routes/rotas');
 
 app.set('port', process.env.PORT || 3333);
 
